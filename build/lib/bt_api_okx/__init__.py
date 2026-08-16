c10fe765e7da68ccbaf9630e27d767486dbe5159
cloudQuant <yunjinqi@gmail.com>
2026-08-16 21:26:17 +0800

    chore: stop tracking build artifacts, ignore pycache/egg-info/build

--- build/lib/bt_api_okx/__init__.py
+++
@@ -1,6 +0,0 @@
-"""OKX plugin package for ``bt_api_py``."""
-
-__version__ = "0.15.0"
-
-__all__ = ["__version__"]
-
--- build/lib/bt_api_okx/gateway/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Gateway shim exports for bt_api_okx."""
-
-from bt_api_okx.gateway.adapter import OkxGatewayAdapter
-
-__all__ = ["OkxGatewayAdapter"]
-
